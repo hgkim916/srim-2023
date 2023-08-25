c5d1b8fdc5bb593ee99163eb1ad1c29b878f3168
ws397 <ws397@cam.ac.uk>
2023-08-25 11:18:59 +0100

    change variable name

--- good_programs/henon/script_create_all_TAH_graphics.py
+++ good_programs/henon/script_create_all_TAH_graphics.py
@@ -6,11 +6,11 @@ from henon_tools_slim import *
 
 d_min = 3
 d_max = 71
-figure_name = "TAH_d_" # Saves as e.g. "TAH_d_19.png" 
+figure_name_prefix = "TAH_d_" # Saves as e.g. "TAH_d_19.png" 
 colour_style = "DEFAULT"
 
 for d in range(d_min,d_max+2,2):     
         create_henon_graphic(discrete_sine_poly(d),int((d+5)/2),int((d+5)/2),
-                             figure_name=figure_name+str(d),
+                             figure_name=figure_name_prefix+str(d),
                              reference_box_size=int((d+5)/2),
                              colour_style=colour_style)
