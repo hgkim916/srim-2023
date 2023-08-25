3663e77e23abbbce549a37c0255431ef8471c99f
ws397 <ws397@cam.ac.uk>
2023-08-25 11:51:26 +0100

    new scripts

--- good_programs/henon/script_create_all_TAH_graphics.py
+++ good_programs/henon/script_create_all_TAH_graphics.py
@@ -7,7 +7,7 @@ from henon_tools_slim import *
 d_min = 3
 d_max = 71
 colour_style = "LENGTH"
-figure_name_prefix = "TAH_"+colour_style+"_d_" # Saves as e.g. "TAH_d_19.png" 
+figure_name_prefix = "TAH_"+colour_style+"_d_" # Saves as e.g. "TAH_LENGTH_d_19.png" 
 
 for d in range(d_min,d_max+2,2):     
         create_henon_graphic(discrete_sine_poly(d),int((d+5)/2),int((d+5)/2),
---
+++ good_programs/henon/script_demo_lexicographic_lists.py
@@ -0,0 +1,10 @@
+from henon_search_for_longer_cycles import next_list_colex_order
+
+list_length = 4
+min_entry = -2
+max_entry = 2
+a = [min_entry]*4
+
+while a != None:
+    print(a)
+    a = next_list_colex_order(a,min_entry,max_entry)
