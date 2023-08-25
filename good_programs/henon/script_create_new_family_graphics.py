9d0001b55841ce8b09166713d85a1093b55a16b1
ws397 <ws397@cam.ac.uk>
2023-08-25 12:37:23 +0100

    adjust scripts

--- good_programs/henon/script_create_new_family_graphics.py
+++ good_programs/henon/script_create_new_family_graphics.py
@@ -2,6 +2,72 @@
 
 from henon_tools_slim import *
 
+def new_family_poly1(d):
+    '''
+    Returns the polynomial of degree d in New Family 1.
+
+    Parameters:
+        d (int): The degree of the polynomial - must be odd.
+    '''
+    if d%2 == 0:
+        raise ValueError("d must be odd.")
+
+    roughly_half = (d-1)//2
+    list_of_values = ([1]*(roughly_half)+[0])*2
+    #print(list_of_values)
+    return make_your_own_function(list_of_values,-roughly_half)
+
+def new_family_poly2(d): # for odd d
+    '''
+    Returns the polynomial of degree d in New Family 2, for odd d.
+
+    Parameters:
+        d (int): The degree of the polynomial - must be odd.
+    '''
+    if d%2 == 0:
+        raise ValueError("d must be odd.")
+    
+    roughly_half = (d-1)//2
+    list_of_values = ([1]*(roughly_half)+[0])*2
+    list_of_values[-2] = -1
+    #print(list_of_values)
+    return make_your_own_function(list_of_values,-roughly_half)
+
+def new_family_poly3(d): # for even d
+    '''
+    Returns the polynomial of degree d in New Family 3, for even d.
+
+    Parameters:
+        d (int): The degree of the polynomial - must be even.
+    '''
+    if d%2 == 1:
+        raise ValueError("d must be even.")
+
+
+    list_of_values = [0]*(d+1)
+    list_of_values[0] = 1
+    list_of_values[d//2] = 1
+    list_of_values[d//2 + 1] = 1
+    return make_your_own_function(list_of_values,-(d//2))
+
+def new_family_poly(d,family_index):
+    '''
+    Returns the polynomial of degree d in the New Family given by family_index.
+    
+    Parameters:
+        d (int): The degree of the polynomial.
+        family_index (int): The index of the New Family. Should be 1, 2 or 3.
+    '''
+    if family_index == 1:
+        return new_family_poly1(d)
+    elif family_index == 2:
+        return new_family_poly2(d)
+    elif family_index == 3:
+        return new_family_poly3(d)
+    else:
+        print("family_index must be 1, 2 or 3")
+        return None
+
 # Change as desired:
 
 family = 1          # choices: 1,2,3.  
@@ -9,7 +75,7 @@ family = 1          # choices: 1,2,3.
 d_min = 3           # Family 2 requires d to be odd, family 3 requires d to be even.
 d_max = 9           # if only one value of d is needed, make d_min=d_max = required value
 colour_style = "LENGTH"
-figure_name_prefix = "new_family_"+str(family)+"_"+colour_style+"_d_" # Saves as e.g. "TAH_LENGTH_d_19.png" 
+figure_name_prefix = "new_family_"+str(family)+"_"+colour_style+"_d_"
 
 for d in range(d_min,d_max+2,2):     
         create_henon_graphic(new_family_poly(d,family),escape_radius=(d+1)//2,check_radius=(d+1)//2,
--- good_programs/henon/script_create_shifted_TAH_graphics.py
+++ good_programs/henon/script_create_shifted_TAH_graphics.py
@@ -2,6 +2,18 @@
 
 from henon_tools_slim import *
 
+def shift_poly_in_x(shift,poly):       
+    '''
+    Shifts the given function to the right, returning poly(x-shift).
+
+    Parameters:
+        shift (int): The distance to shift the function by.
+        poly (function): The function to shift.
+    '''
+    def new_poly(x):
+        return poly(x-shift)
+    return new_poly
+
 # Change as desired:
 
 d_min = 3
@@ -9,7 +21,7 @@ d_max = 9           # if only one value of d is needed, make d_min=d_max = requi
 shift = 2
 step = 6            # by default, only looks at d of the same value mod 6, but can change to any other EVEN step
 colour_style = "LENGTH"
-figure_name_prefix = "shifted_by_"+str(shift)+"_TAH_"+colour_style+"_d_" # Saves as e.g. "TAH_LENGTH_d_19.png" 
+figure_name_prefix = "shifted_by_"+str(shift)+"_TAH_"+colour_style+"_d_"
 
 for d in range(d_min,d_max+2,step):     
         create_henon_graphic(shift_poly_in_x(shift,discrete_sine_poly(d)),int((d+5)/2)+shift,int((d+5)/2)+shift,
